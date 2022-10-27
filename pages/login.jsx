293cc6b3a0dac7cee343d0e8cc4ac8ef92d69ce3
cindyeme <emereninicynthia@gmail.com>
2022-10-27 14:56:37 +0100

    initialized login/register pages

---
+++ pages/login.jsx
@@ -0,0 +1,19 @@
+// import Link from "next/link";
+import Seo from "../components/seo/Seo";
+
+export default function Login() {
+  return (
+    <div className="login">
+      <Seo
+        pageTitle="Polygon | Log in"
+        metaDescriptionContent="Polygon is a User-friendly Automated Machine Learning platform built for novice, researchers , data scientist and machine learning experts"
+      />
+
+      <div className="login__container">
+        <div className="section--pad">
+          
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ pages/register.jsx
@@ -0,0 +1,17 @@
+// import Link from "next/link";
+import Seo from "../components/seo/Seo";
+
+export default function Register() {
+  return (
+    <div className="register">
+      <Seo
+        pageTitle="Polygon | Register"
+        metaDescriptionContent="Polygon is a User-friendly Automated Machine Learning platform built for novice, researchers , data scientist and machine learning experts"
+      />
+
+      <div className="register__container">
+        <div className="section--pad"></div>
+      </div>
+    </div>
+  );
+}
